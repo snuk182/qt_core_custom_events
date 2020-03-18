f95e44ec1665105378ed17f764526a47abae238c
Serhii Plyhun <snuk188@gmail.com>
2020-03-18 19:06:18 +0100

    Rust-Qt 0.5

--- src/custom_event_filter.rs
+++ src/custom_event_filter.rs
@@ -1,20 +1,22 @@
 use cpp_core::*;
+use qt_core::QBox;
+
 /// C++ type: <span style='color: green;'>```QCustomEventFilter```</span>
 #[repr(C)]
 pub struct CustomEventFilter(u8);
 
 impl CustomEventFilter {
-  pub fn new<F>(filter: F) -> CppBox<::custom_event_filter::CustomEventFilter>
+  pub fn new<F>(filter: F) -> QBox<::custom_event_filter::CustomEventFilter>
       where F: for<'a,'b> FnMut(&'a mut ::qt_core::QObject,&'b mut ::qt_core::QEvent) -> bool {
     let boxed_filter: Box<Box<dyn FnMut(&mut ::qt_core::QObject,&mut ::qt_core::QEvent) -> bool>> = Box::new(Box::new(filter));
     let filter_ptr = Box::into_raw(boxed_filter) as *mut _ as *mut ::libc::c_void;
 
     let ffi_result = unsafe { ::ffi::qt_core_c_QCustomEventFilter_new(Some(handler), filter_ptr) };
-    unsafe { CppBox::new(MutPtr::from_raw(ffi_result)).unwrap() }
+    unsafe { QBox::new(Ptr::from_raw(ffi_result)) }
   }
-  pub fn clear(&mut self) {
+  pub fn clear(&self) {
     unsafe {
-      let filter_ptr = ::ffi::qt_core_c_QCustomEventFilter_clear(self as *mut ::custom_event_filter::CustomEventFilter) as *mut Box<dyn FnMut(&mut ::qt_core::QObject,&mut ::qt_core::QEvent) -> bool>;
+      let filter_ptr = ::ffi::qt_core_c_QCustomEventFilter_clear(self as *const _ as *mut ::custom_event_filter::CustomEventFilter) as *mut Box<dyn FnMut(&mut ::qt_core::QObject,&mut ::qt_core::QEvent) -> bool>;
       if !filter_ptr.is_null() {
         let _:Box<Box<dyn FnMut(&mut ::qt_core::QObject,&mut ::qt_core::QEvent) -> bool>>  = Box::from_raw(filter_ptr);
       }
@@ -30,17 +32,12 @@ impl Drop for ::custom_event_filter::CustomEventFilter {
   }
 }
 impl CppDeletable for ::custom_event_filter::CustomEventFilter {
-  unsafe fn delete(&mut self){
+  unsafe fn delete(&self){
       self.clear();
-      ::ffi::qt_core_c_QCustomEventFilter_delete(self as *mut ::custom_event_filter::CustomEventFilter)
+      ::ffi::qt_core_c_QCustomEventFilter_delete(self as *const _ as *mut ::custom_event_filter::CustomEventFilter)
   }
 }
 impl StaticUpcast<::qt_core::QObject> for ::custom_event_filter::CustomEventFilter {
-  unsafe fn static_upcast_mut(ptr: MutPtr<Self>) -> MutPtr<::qt_core::QObject> {
-    let ffi_result = ::ffi::qt_core_c_QCustomEventFilter_G_static_cast_QObject_ptr(ptr.as_mut_raw_ptr() as *mut ::custom_event_filter::CustomEventFilter);
-    MutPtr::from_raw(ffi_result)
-  }
-
   unsafe fn static_upcast(ptr: Ptr<Self>) -> Ptr<::qt_core::QObject> {
     let ffi_result = ::ffi::qt_core_c_QCustomEventFilter_G_static_cast_QObject_ptr(ptr.as_raw_ptr() as *const ::custom_event_filter::CustomEventFilter as *mut ::custom_event_filter::CustomEventFilter);
     Ptr::from_raw(ffi_result)
